5eb0bcf852e498fee98553a656b2486214561a67
Andrej Dudenhefner <andrej.dudenhefner@cs.tu-dortmund.de>
2018-09-24 16:27:25 +0200

    substitute_partial_chain (and prerequisites)

--- SystemF.v
+++ SystemF.v
@@ -16,6 +16,15 @@ Unset Printing Implicit Defensive.
 
 Require Import Term.
 Require Import Derivations.
+Require Import FormulaFacts.
+
+Tactic Notation "move" "//" := 
+  let H := fresh in move => H; 
+  match goal with 
+    | [|- _ -> _] => move /(H) 
+    | [|- forall _, _] => let a := fresh in move => a; move : (H a); (try clear a)
+  end; clear H.
+
 
 (*additional lemmas*)
 Lemma lc_bind : forall (M : term) (n : nat) (x : label), Term.lc n M -> Term.lc (S n) (Term.bind x n M).
@@ -187,10 +196,6 @@ Lemma bind_normal : forall (x : label) (N : term) (n : nat), normal_form (Term.b
 Proof.
 firstorder using bind_normal_and_head, Term.normal_bind.
 Qed.
-
-
-Tactic Notation "move" "//" := let H := fresh in move => H; move /(H); clear H.
-
 
 Fixpoint formula_size (t : formula) :=
   match t with
@@ -416,8 +421,70 @@ intros.
 by apply (Label.eqb_eq a b).
 Qed.
 
-(*MOST IMPORTANT LEMMA*)
-Lemma prerequisive0 : forall (a : label) (s : formula), lc 0 s -> forall (n : nat) (Γ : list formula) (t : formula), 
+Lemma substitute_instantiation2 : forall a u, lc 0 u -> forall t s n, 
+  substitute a u (instantiate s n t) = instantiate (substitute a u s) n (substitute a u t).
+Proof.
+move => a u ?.
+elim; cbn.
+{
+move => n s m. case : (m =? n); by cbn.
+}
+{
+move => b s n.
+case : (Label.eqb a b); cbn => //.
+by rewrite Lc.instantiate_eq0.
+}
+all : intros; f_equal; eauto.
+Qed.
+
+Lemma instantiate_substitution_neq : forall (s t : formula) (a b : label) (n : nat), 
+  a <> b -> lc 0 s ->
+  (instantiate (atom a) n (substitute b s t)) = substitute b s (instantiate (atom a) n t).
+Proof.
+Admitted.
+
+
+Lemma lc_substitute : forall a s t n, lc n s -> lc n t -> lc n (substitute a s t).
+Proof.
+move => a s. elim; cbn => //.
+
+move => b *. by case : (Label.eqb a b).
+
+intros; constructor; gimme lc where arr; inversion; eauto.
+
+intros; constructor; gimme lc where quant; inversion.
+gimme lc where S. gimme where substitute. move //.
+nip. apply : Lc.relax; first eassumption; omega.
+done.
+Qed.
+
+
+Lemma substitute_contains : forall a s t u, 
+  lc 0 u -> contains s t -> contains (substitute a u s) (substitute a u t).
+Proof.
+intros until 0 => ?. 
+elim.
+intros. constructor.
+
+intros. cbn. 
+apply : contains_trans; first last.
+
+rewrite <- substitute_instantiation2; eassumption.
+by apply lc_substitute.
+Qed.
+
+
+Lemma substitute_partial_chain : forall a s t u ts, lc 0 u ->
+  partial_chain s t ts -> partial_chain (substitute a u s) (substitute a u t) (map (substitute a u) ts).
+Proof.
+intros until 0 => ?; elim.
+intros. constructor. by apply substitute_contains.
+intros. cbn. apply : partial_chain_cons; last eassumption.
+gimme contains. by apply /substitute_contains.
+Qed.
+
+(*MOST IMPORTANT LEMMA, old substitute_normal_derivation is only on label basis*)
+Lemma substitute_normal_derivation2 : forall (a : label) (s : formula), lc 0 s -> forall (n : nat) (Γ : list formula) (t : formula), 
   normal_derivation n Γ t -> ∃ n : nat, normal_derivation n (map (substitute a s) Γ) (substitute a s t).
 Proof.
 move => a s ?.
@@ -432,7 +499,15 @@ exists (S n). cbn. by constructor.
 }
 
 {
-admit. (*exchange forall exists*)
+cbn. apply normal_derivation_exists_quant.
+gimme where normal_derivation. move //.
+intro.
+match goal with [_ : normal_derivation _ _ (instantiate (atom ?b') _ _) |- _] => rename b' into b end.
+case : (Label.eq_dec b a).
+admit. (*unwieldy, but possible, maybe need freshness?*)
+gimme lc.
+move /instantiate_substitution_neq. move //. move => ->.
+apply : IH; try eassumption; omega.
 }
 
 {
@@ -470,9 +545,19 @@ intros; gimme contains_depth; inversion.
 eauto.
 
 gimme normal_derivation; inversion.
+have [a ?] := exists_fresh (u :: Γ).
+decompose_Forall.
+gimme where normal_derivation. move /(_ a).
+
 gimme contains_depth; move /IH.
 nip; first omega.
 apply; try eassumption.
+
+constructor.
+apply : Lc.instantiate_pred => //.
+gimme well_formed_formula where quant. inversion.
+gimme lc. inversion. assumption.
+
 admit. (*easy*)
 
 Admitted.
@@ -512,6 +597,13 @@ move => [? s] Γ IH. inversion.
 constructor; eauto.
 Qed.
 
+(*
+Lemma letstry : forall M Γ t, head_form M -> f_derivation Γ M t -> exists x s ts, In (x, s) Γ /\ partial_chain s t ts.
+Proof.
+move => M.
+move : (term_depth <)
+*)
+
 (*TODO, main lemma*)
 Lemma f_derivation_soundness : forall (Γ : environment) (M : term) (t : formula), 
   f_derivation Γ M t -> normal_form M -> exists (n : nat), normal_derivation n (map snd Γ) t.
@@ -528,7 +620,8 @@ constructor.
 }
 
 { (*case app*)
-intros; gimme normal_form; inversion; gimme head_form; inversion.
+intros. gimme normal_form. inversion.
+gimme head_form. inversion. 
 admit.
 }
 
@@ -565,333 +658,3 @@ rewrite <- map_substitute_fresh_label; last by apply fresh_formula_label_Forall.
 eauto.
 }
 Qed.
-
-head_form M
-normal_form N
-
-Inductive normal_derivation : nat -> list formula -> formula -> Prop :=
-(*(s :: Γ) in derive_arr not a problem, context permutation is admissible, think bottom-up*)
-  | derive_arr : forall (n : nat) (Γ: list formula) (s t: formula), 
-    normal_derivation n (s :: Γ) t -> normal_derivation (S n) Γ (Formula.arr s t)
-  | derive_quant : forall (n : nat) (Γ: list formula) (s: formula), 
-   (forall (a: label), normal_derivation n Γ (instantiate (atom a) 0 s) ) -> normal_derivation (S n) Γ (Formula.quant s)
-(* | derive_atom : forall (n : nat) (Γ: list formula) (a: label) (s: formula) (params: list formula), 
-      In s Γ -> Formula.chain s params a -> (Forall (normal_derivation n Γ) (params)) -> normal_derivation (if params is nil then n else S n) Γ (Formula.atom a).*)
-  | derive_atom : forall (n : nat) (Γ: list formula) (a: label) (s: formula) (params: list formula), 
-      In s Γ -> Formula.chain s a params -> (Forall (normal_derivation (Nat.pred n) Γ) (params)) -> (n <> 0 \/ params = nil) -> normal_derivation n Γ (Formula.atom a).
-
-Conjecture normalization : forall (Γ: list formula) (s: formula), derivation Γ s -> exists (n : nat), normal_derivation n Γ s.
-
-(*tries to solve derivation Γ s automatically*)
-Ltac derivation_rule := first
-  [ apply ax => //; by list_element
-  | ( do ? (apply intro_quant => //=; intro);
-      do ? (apply intro_arr);
-      apply ax => //; by list_element)
-  | match goal with | [H : In ?s ?Γ |- derivation _ ?s] => apply ax; list_inclusion end
-  | (by (eauto using derivation))].
-
-
-Theorem conservativity : forall (n : nat) (Γ: list formula) (s: formula), normal_derivation n Γ s -> derivation Γ s.
-Proof.
-elim; intros until 0.
-
-(*base case n = 0*)
-inversion. intuition. subst.
-gimme chain; inversion; derivation_rule.
-
-(*inductive case n > 0*)
-move => IH Γ *.
-gimme normal_derivation; inversion; auto using intro_arr, intro_quant.
-
-(*atom case*)
-gimme In; move /ax.
-gimme Forall;
-gimme chain; elim.
-(*one step chain*) derivation_rule.
-(*multistep chain*) move => ? ? t u *.
-decompose_Forall.
-
-have ? : derivation Γ u by derivation_rule.
-auto.
-Qed.
-
-(*inversion lemmas*)
-Lemma inv_arr : forall (Γ : list formula) (s t : formula),
-  derivation Γ (arr s t) -> derivation (s :: Γ) t.
-Proof.
-intros until 0.
-move /normalization => [? H].
-inversion_clear H.
-apply: conservativity; eassumption.
-Qed.
-
-Lemma inv_atom : forall (Γ : list formula) (a : label), derivation Γ (atom a) -> 
-  exists (s : formula) (params : list formula), In s Γ /\ chain s a params /\ (Forall (derivation Γ) (params)).
-Proof.
-intros until 0.
-move /normalization => [? H].
-inversion_clear H.
-match goal with | [H : Forall _ _ |- _] => eapply Forall_impl in H; first last end.
-intros.
-eapply conservativity. eassumption.
-exists s, params. auto.
-Qed.
-
-Lemma inv_normal_quant : forall (n : nat) (Γ: list formula) (s : formula), normal_derivation n Γ (quant s) ->
-  exists (m : nat), n = S m /\ (forall (a: label), normal_derivation m Γ (instantiate (atom a) 0 s)).
-Proof.
-intros until 0 => H.
-inversion_clear H.
-eexists; split; [reflexivity | assumption].
-Qed.
-
-Lemma inv_quant : forall (Γ: list formula) (s : formula), derivation Γ (quant s) ->
-  (forall (a: label), derivation Γ (instantiate (atom a) 0 s)).
-Proof.
-intros until 0.
-move /normalization => [n HD].
-move /inv_normal_quant : HD => [m [? ?]].
-eauto using conservativity.
-Qed.
-
-Lemma inv_normal_arr : forall (n : nat) (Γ: list formula) (s t : formula), normal_derivation n Γ (arr s t) ->
-  exists (m : nat), n = S m /\ normal_derivation m (s :: Γ) t.
-Proof.
-intros until 0 => H.
-inversion_clear H.
-eexists; split; [reflexivity | assumption].
-Qed.
-
-(*decomposition tactics*)
-Ltac decompose_normal_derivation := 
-  do ? (
-  lazymatch goal with
-  | [H : Forall _ (_ :: _) |- _] => inversion_clear H
-  | [H : Forall _ nil |- _] => inversion_clear H
-  | [H : normal_derivation _ _ (quant _) |- _ ] => 
-    let n := fresh "n" in move /inv_normal_quant : H => [n [? H]]
-  | [H : normal_derivation _ _ (arr _ _) |- _ ] => 
-    let n := fresh "n" in move /inv_normal_arr : H => [n [? H]]
-  | [H : normal_derivation _ _ _ |- _] => inversion_clear H
-  end).
-
-Ltac decompose_chain :=
-  do ? (
-  match goal with
-  | [H : chain ?s _ _ |- _] =>
-    match eval hnf in s with
-    | arr _ _ => (move /chain_arr : H => [? [? H]]); subst
-    | atom _ => (move /chain_atom : H => [? ?]); try done; subst
-    end
-  end).
-
-Ltac decompose_derivation := 
-  do ? (
-  match goal with
-  | [H : derivation _ ?s |- _] => 
-    match eval hnf in s with
-    | arr _ _ => move /inv_arr : H => H
-    | atom _ => 
-      let s := fresh in 
-      let H' := fresh in
-      move /inv_atom : H => [s [? [H' [? ?]]]]
-    end
-  end).
-
-Ltac decompose_contains :=
-  match goal with
-  | [H : contains _ _ |- _] => inversion_clear H
-  end.
-
-
-Ltac decompose_lc := 
-  do ? (
-  match goal with
-  | [H : lc _ ?s |- _] => 
-    match eval hnf in s with
-    | arr _ _ => inversion_clear H
-    | var _ => inversion_clear H
-    | atom _ => inversion_clear H
-    end
-  end).
-
-Tactic Notation "gimme" "where" constr(p) := 
-  lazymatch goal with
-  | [H : context[p] |- _] => move : H
-  end.
-
-Lemma substitute_normal_derivation : forall (n : nat) (s : formula) (Γ : list formula) (a b : label), 
-  normal_derivation n Γ s -> normal_derivation n (map (substitute_label a b) Γ) (substitute_label a b s).
-Proof.
-move => n; apply (lt_wf_ind n).
-
-intros until 0 => IH; intros.
-
-gimme normal_derivation; inversion; cbn.
-(*arr*)
-apply : derive_arr.
-rewrite <- map_cons.
-apply : IH; try done + omega.
-(*quant*)
-match goal with [_ : context G [instantiate _ _ ?s'] |- _] => rename s' into s end.
-apply : derive_quant => c.
-case : (Label.eq_dec c a).
-
-intro; subst c.
-have [d Hd] := exists_fresh ([atom a; atom b; s] ++ (map (substitute_label a b) Γ)).
-decompose_Forall.
-
-do 2 (gimme shape (fresh_in d (atom _)); inversion).
-rewrite -> (@instantiate_renaming_eq _ _ _ d); auto.
-rewrite -> (@map_substitute_fresh_label d a (map (substitute_label a b) Γ)); auto.
-
-intros.
-rewrite -> instantiate_renaming_neq; auto.
-
-(*atom*)
-set Γ' := map (substitute_label a b) Γ.
-match goal with [_ : In ?s' Γ |- _] => rename s' into s end.
-set s' := (substitute_label a b) s.
-rewrite if_fun.
-match goal with [_ : chain s ?a' params |- _] => rename a' into c end.
-set a' := (if Label.eqb a c then b else c).
-set params' := map (substitute_label a b) params.
-have ? : In s' Γ' by apply : in_map; assumption.
-have ? : chain s' a' params' by apply : substitute_chain; assumption.
-
-have ? : Forall (normal_derivation (Nat.pred n0) Γ') params'.
-gimme Forall; move : IH; gimme or; clear; revert dependent params.
-elim; cbn; first done.
-
-intros; decompose_Forall.
-gimme or; case; last done; move => ?.
-match goal with [_ : ?n' ≠ 0 |- _] => have ? : Nat.pred n' < n' by omega end.
-eauto.
-
-apply : derive_atom; try eassumption.
-gimme or; case => ?; last subst params' params; auto.
-Qed.
-
-
-Lemma substitute_derivation_bindable : forall (s : formula) (Γ : list formula) (a b : label), 
-  Forall (fresh_in a) Γ -> derivation Γ s -> derivation Γ (substitute_label a b s).
-Proof.
-intros.
-have : Γ = map (substitute_label a b) Γ by apply map_substitute_fresh_label.
-move => ->.
-gimme derivation; move /normalization => [? ?].
-eauto using conservativity, substitute_normal_derivation.
-Qed.
-
-
-(*the usual presentation of intro_quant*)
-Theorem intro_quant_fresh : forall (s: formula) (Γ : list formula) (a : label), 
-  Forall (fresh_in a) Γ -> fresh_in a s ->
-  (derivation Γ (instantiate (atom a) 0 s)) -> derivation Γ (Formula.quant s).
-Proof.
-move => s Γ a H *.
-apply intro_quant => b.
-gimme derivation.
-move /(substitute_derivation_bindable b H).
-rewrite -> rename_instantiation; auto.
-Qed.
-
-
-Lemma normal_weakening : forall (n : nat) (Γ Δ: list formula), 
-  (forall (s : formula), In s Γ -> In s Δ) -> forall (t: formula), normal_derivation n Γ t -> normal_derivation n Δ t.
-Proof.
-elim.
-intros until 0 => ? ?; inversion.
-intuition; subst.
-apply: derive_atom; eauto.
-
-move => n IH Γ Δ H_in t; inversion.
-(*case arr*)
-apply: derive_arr. apply: IH; try eassumption.
-move => s'. move : (H_in s').
-list_inclusion.
-
-(*case quant*)
-apply: derive_quant. eauto.
-(*case atom*)
-apply: derive_atom; eauto.
-gimme Forall; rewrite -> ? Forall_forall.
-eauto.
-Qed.
-
-
-Lemma weakening : forall (Γ Δ: list formula) (t: formula), 
-  derivation Γ t -> (forall (s : formula), In s Γ -> In s Δ) -> derivation Δ t.
-Proof.
-intros until 0.
-move /normalization; case.
-eauto using conservativity, normal_weakening.
-Qed.
-
-
-Lemma weakening_cons : forall (Γ: list formula) (s t: formula), derivation Γ t -> derivation (s :: Γ) t.
-Proof.
-firstorder using weakening.
-Qed.
-
-
-Lemma context_generalization : forall (Γ Δ: list formula) (t : formula), 
-  derivation Γ t -> (forall (s : formula), In s Γ -> derivation Δ s) -> derivation Δ t.
-Proof.
-elim.
-(*base case*)
-intros.
-apply: (weakening (Γ := [])) => //.
-(*inductive case*)
-intros until 0 => IH.
-intros until 1 => H'.
-have ? : derivation l (arr a t) by derivation_rule.
-have ? : derivation Δ (arr a t) => //.
-apply IH => //; intros; apply H'; list_inclusion.
-have ? : derivation Δ a by apply H'; list_inclusion.
-derivation_rule.
-Qed.
-
-
-Lemma context_generalization_app : forall (Γ Δ Γ': list formula) (t : formula), 
-  derivation (Γ ++ Δ) t -> (forall (u : formula), In u Γ -> derivation Γ' u) -> derivation (Γ' ++ Δ) t.
-Proof.
-intros.
-apply: (context_generalization). eassumption.
-move => s H_In.
-apply in_app_or in H_In.
-case : H_In => H_In.
-apply : (weakening (Γ := Γ')); auto with *.
-derivation_rule.
-Qed.
-
-
-(*looks for In/chain statements and eliminates impossible cases*)
-Ltac filter_context_chain := 
-  let filter_context_chain_inner H_In :=
-    move => H_In; subst => //;
-    (do ? (
-      match goal with 
-      | [|- chain ?s _ _ -> _] => 
-        match eval hnf in s with
-        | (arr _ _) => (let H' := fresh in move /chain_arr => [? [? H']]; move: H'; subst => //)
-        | (atom _) => (move /chain_atom => [? ?]; subst => //)
-        end
-      end))
-  in
-    match goal with | [ H_In : In ?s _, H_c : chain ?s _ _ |- _] => move: H_In H_c;
-    rewrite ? (in_app_iff, in_cons_iff, or_assoc);
-    let t := (filter_context_chain_inner H_In) in decompose_or t end.
-
-
-(*looks for In statements to derive current goal and eliminates trivial cases*)
-Ltac filter_context_derivation := 
-  let rec filter_context_derivation_inner H_In :=
-    move => H_In; subst; try derivation_rule
-  in
-  match goal with | [ H_In : In ?s _ |- derivation _ ?s] => 
-    move: H_In;
-    rewrite ? (in_app_iff, in_cons_iff, or_assoc);
-    let t := filter_context_derivation_inner H_In in decompose_or t
-  end.
